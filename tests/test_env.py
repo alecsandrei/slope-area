392a6e5c50bc3e7c65564bdfef981be222cc91c5
Alex Andrei Cuvuliuc <cuvuliucalexandrei@gmail.com>
2025-10-28 16:20:03 +0200

    update tests

--- tests/test_env.py
+++ tests/test_env.py
@@ -1,15 +1,16 @@
 from __future__ import annotations
 
-from slope_area import SAGA_ENV, WBW_ENV
+from slope_area.config import get_saga_env, get_wbw_env
 
 
 def test_saga_env():
-    saga_version = SAGA_ENV.version
+    saga_version = get_saga_env().version
     assert saga_version.major >= 7, 'Minimum SAGA GIS version should be 7.*'
 
 
 def test_wbw_env():
-    version = WBW_ENV.version
-    verbose = WBW_ENV.verbose
+    wbw_env = get_wbw_env()
+    version = wbw_env.version
+    verbose = wbw_env.verbose
     assert version is not None
     assert not verbose
