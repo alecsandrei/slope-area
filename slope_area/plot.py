89b0cf1a3208aa4ed18cc70740fc6f8cde82ee18
Alex Andrei Cuvuliuc <cuvuliucalexandrei@gmail.com>
2025-10-29 11:33:13 +0200

    make plots more configurable

--- slope_area/plot.py
+++ slope_area/plot.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
+from functools import partial
 from itertools import groupby
 from pathlib import Path
 
+from matplotlib.axes import Axes
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -15,30 +17,34 @@ from slope_area.logger import create_logger
 m_logger = create_logger(__name__)
 
 
-def slope_area_plot(
-    area: np.ndarray,
-    slope: np.ndarray,
-    min_gradient: float = 0.01,
-    log_interval: float = 0.25,
-    add_vlines: bool = False,
-    add_scatter: bool = False,
+def slope_area_plot_func(
+    area_col: str,
+    slope_col: str,
+    data: pd.DataFrame,
+    config: SlopeAreaPlotConfig,
     ymin: float | None = None,
     ymax: float | None = None,
-    kind: PlotKind = 'line',
+    ax: Axes | None = None,
     **plot_kwargs,
 ):
-    slope = np.clip(slope, a_min=min_gradient, a_max=None)
+    if ax is not None:
+        plt.sca(ax)
+    area: pd.DataFrame = data[area_col]
+    slope: pd.DataFrame = data[slope_col]
+    slope = np.clip(slope, a_min=config.min_gradient, a_max=None)
     areamin = area.min() - 0.01
     areamax = area.max() + 1
     area_bins = 10 ** np.arange(
-        np.log10(areamin), np.log10(areamax) + log_interval, log_interval
+        np.log10(areamin),
+        np.log10(areamax) + config.log_interval,
+        config.log_interval,
     )
     half_bin_widths = np.append(np.diff(area_bins) / 2, 0)
     acenters = area_bins + half_bin_widths
     bin_indices = np.digitize(area, area_bins, right=True)
     data = np.column_stack([slope, area, bin_indices])
     data = data[data[:, 2].argsort()]
-    if kind == 'line':
+    if config.kind == 'line':
         mean_slopes = []
         for bin, grouper in groupby(data, lambda arr: arr[2]):
             mean = float(np.mean([values[0] for values in grouper]))
@@ -53,19 +59,19 @@ def slope_area_plot(
             markersize=5,
             **plot_kwargs,
         )
-    elif kind == 'scatter':
+    elif config.kind == 'scatter':
         sns.scatterplot(
             x='area',
             y='slope',
             data=pd.DataFrame(data, columns=['slope', 'area', 'bin']),
             s=10,
-            alpha=0.1,
+            alpha=0.3,
             edgecolors='black',
             linewidths=0.2,
             **plot_kwargs,
         )
 
-    if add_vlines:
+    if config.add_vlines:
         plt.vlines(
             area_bins,
             ymin=ymin or slope.min(),
@@ -73,7 +79,6 @@ def slope_area_plot(
             linewidth=0.5,
             colors='cyan',
         )
-    plt.tick_params(labelsize=14)
     plt.grid(True, which='both', linestyle='--', linewidth=0.5)
 
 
@@ -90,50 +95,133 @@ def get_col_wrap(n_unique: int):
 
 @dataclass(kw_only=True)
 class SlopeAreaPlotConfig:
+    hue: str | None = None
+    col: str | None = None
     log_interval: float = 0.25
     min_gradient: float = 0.01
     col_wrap: int = -1
     height: int = 5
     aspect: int = 1
+    title: str | None = None
+    xlabel: str = 'Drainage area (m$^2$)'
+    ylabel: str = 'Slope (m/m)'
+    label_font_size: float = 16
+    title_font_size: float = 10
+    legend_font_size: float = 10
+    tick_font_size: float = 14
     add_vlines: bool = False
     kind: PlotKind = 'line'
     show: bool = False
 
 
+def set_plot_options(config: SlopeAreaPlotConfig, ax: Axes) -> None:
+    ax.set_xlabel(config.xlabel, fontdict={'size': config.label_font_size})
+    ax.set_ylabel(config.ylabel, fontdict={'size': config.label_font_size})
+    ax.set_title(config.title or '', fontdict={'size': config.title_font_size})
+    ax.tick_params(labelsize=config.tick_font_size)
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.grid(True, which='both', linestyle='--', linewidth=0.5)
+    ax.set_box_aspect(1)
+
+    if config.hue is not None:
+        ax.legend(fontsize=config.legend_font_size)
+
+    plt.tight_layout(pad=2)
+
+
+def set_plot_options_facetgrid(
+    config: SlopeAreaPlotConfig, facet_grid: sns.FacetGrid
+) -> None:
+    facet_grid.set_xlabels(
+        config.xlabel, fontdict={'size': config.label_font_size}
+    )
+    facet_grid.set_ylabels(
+        config.ylabel, fontdict={'size': config.label_font_size}
+    )
+    facet_grid.set_titles(
+        config.title, fontdict={'size': config.title_font_size}
+    )
+    facet_grid.tick_params(labelsize=config.tick_font_size)
+    facet_grid.set(xscale='log', yscale='log')
+    plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+
+    if config.hue is not None:
+        facet_grid.axes.flat[0].legend(fontsize=config.legend_font_size)
+
+    plt.tight_layout(pad=2)
+
+
 def slope_area_grid(
-    data: pd.DataFrame, col: str, out_fig: Path, config: SlopeAreaPlotConfig
-):
+    data: pd.DataFrame,
+    out_fig: Path,
+    config: SlopeAreaPlotConfig | None = None,
+) -> sns.FacetGrid:
+    if config is None:
+        config = SlopeAreaPlotConfig()
     m_logger.info('Creating slope area plot with config %s' % config)
     if config.col_wrap == -1:
-        col_wrap = get_col_wrap(data[col].nunique())
+        if config.col is not None:
+            col_wrap = get_col_wrap(data[config.col].nunique())
+        else:
+            col_wrap = 1
     else:
         col_wrap = config.col_wrap
     g = sns.FacetGrid(
         data,
-        hue='slope_type',
-        col=col,
+        hue=config.hue,
+        col=config.col,
         height=config.height,
         aspect=config.aspect,
         col_wrap=col_wrap,
     )
     slope = data['slope']
-    g = g.map(
-        slope_area_plot,
+    g = g.map_dataframe(
+        slope_area_plot_func,
         'area',
         'slope',
-        min_gradient=config.min_gradient,
-        log_interval=config.log_interval,
-        add_vlines=config.add_vlines,
-        kind=config.kind,
+        config=config,
         ymin=slope.min(),
         ymax=slope.max(),
     ).set(xscale='log', yscale='log')
-    g.set_axis_labels('Drainage area (m$^2$)', 'Slope (m/m)', fontsize=16)
-    g.set_titles(size=10)
-    g.axes.flat[0].legend(fontsize=10)
-    plt.tight_layout(pad=2)
+    set_plot_options_facetgrid(config, g)
     plt.savefig(out_fig, dpi=300)
     m_logger.info('Saved slope area plot at %s' % out_fig)
+    return g
+
+
+def slope_area_plot_single(
+    data: pd.DataFrame,
+    out_fig: Path,
+    config: SlopeAreaPlotConfig,
+    ax: Axes | None = None,
+) -> Axes:
+    if ax is None:
+        fig, ax = plt.subplots()
+    func = partial(slope_area_plot_func, 'area', 'slope', config=config, ax=ax)
+    if config.hue is not None:
+        for grouper, group in data.groupby(config.hue):
+            func(label=grouper, data=group)
+    else:
+        func(data=data)
+    set_plot_options(config, ax)
+    plt.savefig(out_fig, dpi=300)
+    m_logger.info('Saved slope area plot at %s' % out_fig)
+    return ax
+
+
+def slope_area_plot(
+    data: pd.DataFrame,
+    out_fig: Path,
+    config: SlopeAreaPlotConfig | None = None,
+    ax: Axes | None = None,
+) -> sns.FacetGrid | Axes:
+    if config is None:
+        config = SlopeAreaPlotConfig()
+    if config.col is None:
+        ret = slope_area_plot_single(data, out_fig, config, ax=ax)
+    else:
+        ret = slope_area_grid(data, out_fig, config)
     if config.show:
         plt.show()
-    plt.close()
+    return ret
