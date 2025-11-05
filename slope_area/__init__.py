c735a79c893bb269df1d4bc7ba3e0f39dec267cc
Alex Andrei Cuvuliuc <cuvuliucalexandrei@gmail.com>
2025-11-05 11:41:40 +0200

    add __all__ in __init__

--- slope_area/__init__.py
+++ slope_area/__init__.py
@@ -1,5 +1,62 @@
 from __future__ import annotations
 
+from slope_area.config import get_saga_env, get_wbw_env
+from slope_area.enums import Column, SlopeAreaMethod, Verbose
+from slope_area.features import Outlet, Outlets, Raster
+from slope_area.geomorphometry import DefaultSlopeProviders
 import slope_area.logger
+from slope_area.logger import set_verbose
+from slope_area.plot import SlopeAreaPlotConfig, slope_area_plot
+from slope_area.trial import (
+    AnalysisConfig,
+    HydrologicAnalysisConfig,
+    OutletTrialFactory,
+    ResolutionTrialFactory,
+    Trial,
+    TrialContext,
+    TrialData,
+    TrialFactory,
+    TrialFactoryContext,
+    TrialResult,
+    TrialResults,
+    Trials,
+    TrialsExecutor,
+)
 
 slope_area.logger.setup_logging()
+
+__all__ = [
+    # enums
+    'Column',
+    'SlopeAreaMethod',
+    'Verbose',
+    # features
+    'Outlet',
+    'Outlets',
+    'Raster',
+    # geomorphometry
+    'DefaultSlopeProviders',
+    # logger
+    'slope_area.logger',
+    'set_verbose',
+    # plot
+    'SlopeAreaPlotConfig',
+    'slope_area_plot',
+    # trial
+    'AnalysisConfig',
+    'HydrologicAnalysisConfig',
+    'OutletTrialFactory',
+    'ResolutionTrialFactory',
+    'Trial',
+    'TrialContext',
+    'TrialData',
+    'TrialFactory',
+    'TrialFactoryContext',
+    'TrialResult',
+    'TrialResults',
+    'Trials',
+    'TrialsExecutor',
+    # config
+    'get_saga_env',
+    'get_wbw_env',
+]
